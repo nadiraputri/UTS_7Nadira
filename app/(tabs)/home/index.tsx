aae1783fb3c77ee7e2b50f5979489773eb15d8fa
nadirahijriani26 <nadirahijriani26@gmail.com>
2024-12-24 11:07:20 +0700

    ngupdate warna

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#FFE6A5',
     padding: 20,
   },
   logo: {
